6e443d0604aeb3ec7050f963b964b16a4eee1084
wineynia <gabiybiy@gmail.com>
2025-11-04 21:07:08 +0100

    Fix the html

---
+++ js/gameText.js
@@ -0,0 +1,6 @@
+alert("🌿🌿🌿 Welcome to the Borneo Jungle Adventure!🌿🌿🌿");
+
+const adventurer = prompt("What is your name, adventurer?");
+let health = 3;
+
+alert(`Salamat datang, ${adventurer} ! You have ${health} hearts ❤️❤️❤️`);
